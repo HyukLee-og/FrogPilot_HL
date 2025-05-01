61f9a911d43180b4ae8509c8c1090567ec889aa2
leehyuk1108 <leehyuk03@gmail.com>
2025-05-01 19:40:04 +0900

    registration skip

--- system/athena/registration.py
+++ system/athena/registration.py
@@ -94,8 +94,9 @@ def register(show_spinner=False) -> str | None:
       spinner.close()
 
   if dongle_id:
-    params.put("DongleId", dongle_id)
-    set_offroad_alert("Offroad_UnofficialHardware", (dongle_id == UNREGISTERED_DONGLE_ID) and not PC)
+    # params.put("DongleId", dongle_id)
+    # set_offroad_alert("Offroad_UnofficialHardware", (dongle_id == UNREGISTERED_DONGLE_ID) and not PC)
+    pass
   return dongle_id
 
 
