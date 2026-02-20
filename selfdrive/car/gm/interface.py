a70ce96a964eb250ef6a352fdd3ff1fa37ddd248
leehyuk1108 <leehyuk03@gmail.com>
2026-02-20 15:19:03 +0900

    ver1.7.2: Fix cruise speed display for GM ASCM with stock long

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -149,7 +149,7 @@ class CarInterface(CarInterfaceBase):
       ret.openpilotLongitudinalControl = not disable_openpilot_long
       ret.networkLocation = NetworkLocation.gateway
       ret.radarUnavailable = RADAR_HEADER_MSG not in fingerprint[CanBus.OBSTACLE] and not docs
-      ret.pcmCruise = False  # stock non-adaptive cruise control is kept off
+      ret.pcmCruise = not ret.openpilotLongitudinalControl  # stock non-adaptive cruise control is kept off if OP long is enabled
       # supports stop and go, but initial engage must (conservatively) be above 18mph
       ret.minEnableSpeed = 18 * CV.MPH_TO_MS
       ret.minSteerSpeed = 7 * CV.MPH_TO_MS
